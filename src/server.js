7912b0d8ca90555e42ab6d3fbf3c672da18eff27
ColeyG <cjgeerts@gmail.com>
2020-02-03 16:27:51 -0500

    Initial Nav

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,8 +14,22 @@ httpServer.listen(3000, () => {
 
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '../public')));
+
+const getTopLevelNav = () => {
+  const files = fs.readdirSync('./html');
+  const nav = [];
+  files.forEach((file) => {
+    if (file.includes('.html')) {
+      nav.push({ file: file.replace('.html', ''), type: 'file' });
+    } else if (fs.existsSync(`./html/${file}`) && file !== '.gitkeep') {
+      nav.push({ file, type: 'dir' });
+    }
+  });
+  return nav;
+};
 
 app.get('/', (req, res, next) => {
   res.status(200);
-  res.render('index');
+  res.render('index', { nav: getTopLevelNav() });
 });
