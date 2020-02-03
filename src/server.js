ea37ad150b0672df9c4ee6f139958fc488b8c599
ColeyG <cjgeerts@gmail.com>
2020-02-03 15:44:33 -0500

    Update Server Location

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ httpServer.listen(3000, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
