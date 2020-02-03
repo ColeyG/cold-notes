8b1ba4774e095aa8847af6834e9ea5f4fe68c0a4
ColeyG <cjgeerts@gmail.com>
2020-02-03 15:36:44 -0500

    Express EJS Rendering

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const httpServer = http.createServer(app);
+
+httpServer.listen(3000, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+  res.status(200);
+  res.render('index');
+});
