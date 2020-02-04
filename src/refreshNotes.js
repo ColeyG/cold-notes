dd1c9b1c5c3bdc026f897c9a5ace3ed24b350ccb
ColeyG <cjgeerts@gmail.com>
2020-02-03 21:19:05 -0500

    Moving Config

--- src/refreshNotes.js
+++ src/refreshNotes.js
@@ -1,4 +1,4 @@
 const { exec } = require('child_process');
-const config = require('../config.json');
+const config = require('../config/config.json');
 
 const clone = exec(`git clone ${config.repo} notes`);
